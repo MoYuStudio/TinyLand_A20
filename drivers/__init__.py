fd3c8f1ed55d96d175f4ae9807882d1e3eca21ec
WilsonVinson <cprewilsonvinson@gmail.com>
2023-09-05 10:31:38 +0800

    main

--- drivers/__init__.py
+++ drivers/__init__.py
@@ -1,4 +1,5 @@
 
 from . import csv
 from . import json
+from . import sqlite
 from . import yaml
--- drivers/sqlite/sqlite_driver.py
+++ drivers/sqlite/sqlite_driver.py
@@ -5,24 +5,65 @@ import sqlite3
 class SQLiteDriver:
     '''
     SQLite 数据库读取和写入模块
+    
+    这个模块提供了与 SQLite 数据库的交互功能，包括连接、断开连接、读取和写入数据。
     '''
+
     def __init__(self):
+        '''
+        初始化 SQLiteDriver 类。
+        使用指定的数据库文件路径和默认的表名进行初始化。
+        '''
         self.path = 'data/data.db'
+        self.table_name = 'block'
     
     def connect(self):
+        '''
+        连接到 SQLite 数据库。
+        打开与指定路径的数据库文件的连接并创建游标。
+        '''
         self.db_connection = sqlite3.connect(self.path)
         self.cursor = self.db_connection.cursor()
         
     def disconnect(self):
+        '''
+        断开与数据库的连接。
+        关闭游标和数据库连接。
+        '''
         self.cursor.close()
         self.db_connection.close()
+        
+    def get_table_columns(self):
+        '''
+        获取表的列名。
+        使用 PRAGMA 语句查询指定表的列信息，并返回列名列表。
+        '''
+        self.connect()
+        
+        columns = []
+        
+        query = f"PRAGMA table_info({self.table_name})"
+        self.cursor.execute(query)
+        
+        column_info = self.cursor.fetchall()
+        for info in column_info:
+            column_name = info[1]
+            columns.append(column_name)
+            
+        self.disconnect()
+        
+        return columns
     
     def read_all(self):
+        '''
+        读取表中的所有数据。
+        执行 SELECT 查询来获取指定表中的所有数据，并以字典形式返回结果。
+        '''
         self.connect()
         
         data = {}
         
-        self.cursor.execute("SELECT * FROM block")
+        self.cursor.execute(f"SELECT * FROM {self.table_name}")
         all_rows = self.cursor.fetchall()
         for row in all_rows:
             local_row = []
@@ -39,10 +80,16 @@ class SQLiteDriver:
         return data
 
     def write_all(self):
+        '''
+        尚未实现的方法，用于将数据写入数据库。
+        '''
         pass
 
 if __name__ == '__main__':
     sqlite_driver = SQLiteDriver()
+    
+    columns = sqlite_driver.get_table_columns()
+    print(columns)
     
     data = sqlite_driver.read_all()
     print(data)
